da99fd8c55b313119c77328e165d1921b3755d2d
captainmayo <reventon1911@gmail.com>
2023-06-24 23:39:41 +0700

    add Toast, update readme

--- src/components/chat.tsx
+++ src/components/chat.tsx
@@ -5,6 +5,7 @@ import { ChatPanel } from './chat-panel';
 import { EmptyScreen } from './onboarding';
 import { ChatMessage } from './chat-message';
 import { ChatScrollAnchor } from './chat-scroll-anchor';
+import { toast } from 'react-hot-toast';
 
 export function Chat() {
   const {
@@ -15,34 +16,15 @@ export function Chat() {
     stop,
     reload,
     isLoading,
-  } = useChat();
+  } = useChat({
+    onResponse(response) {
+      if (response.status === 401) {
+        toast.error(response.statusText);
+      }
+    },
+  });
 
   return (
-    // <div className="flex flex-col mx-auto min-h-screen max-w-3xl px-6  md:max-w-5xl pt-12 pb-24">
-    //   {messages.length > 0 ? (
-    // <div className="relative mx-auto max-w-2xl px-4">
-    //   {messages.map((message, index) => (
-    //     <div key={index}>
-    //       <ChatMessage message={message} />
-    //       {index < messages.length - 1 && (
-    //         <hr className="my-12 h-0.5 border-t-0 bg-neutral-100 opacity-100 dark:opacity-50" />
-    //       )}
-    //     </div>
-    //   ))}
-    // </div>
-    //   ) : (
-    //     <EmptyScreen />
-    //   )}
-    //   <ChatPanel
-    //     isLoading={isLoading}
-    //     stop={stop}
-    //     reload={reload}
-    //     messages={messages}
-    //     input={input}
-    //     handleInputChange={handleInputChange}
-    //     handleSubmit={handleSubmit}
-    //   />
-    // </div>
     <>
       <div className="pb-[200px] pt-4 md:pt-10">
         {messages.length > 0 ? (
